7c1dcee36fccc419b7168e95d99caae83f44a69b
Couapy <mael@marchand.cloud>
2020-11-02 18:35:55 +0100

    Update polybar configuration

--- .config/polybar/scripts/redshift.sh
+++ .config/polybar/scripts/redshift.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# Specifying the icon(s) in the script
-# This allows us to change its appearance conditionally
-icon=""
+ICON_BAR=""
 
-pgrep -x redshift &> /dev/null
-if [[ $? -eq 0 ]]; then
-    temp=$(redshift -p 2>/dev/null | grep Temp | cut -d':' -f2)
+if [[ $(pgrep -x redshift > /dev/null) -eq 0 ]]; then
+    temp=$(redshift -p 2>/dev/null | grep temp | cut -d":" -f2 | cut -d" " -f2)
 fi
 
 if [[ -n $temp ]]; then
-    echo " ${icon} ${temp}"
+    echo "${ICON_BAR} ${temp}"
 fi
--- .config/polybar/scripts/updates.sh
+++ .config/polybar/scripts/updates.sh
@@ -12,13 +12,13 @@ while true; do
     if hash notify-send &>/dev/null; then
         if (( UPDATES > 50 )); then
             notify-send -u critical -i $NOTIFY_ICON \
-                "You really need to update!!" "$UPDATES New packages"
+                "You really need to update!!" "${UPDATES} New packages"
         elif (( UPDATES > 25 )); then
             notify-send -u normal -i $NOTIFY_ICON \
-                "You should update soon" "$UPDATES New packages"
+                "You should update soon" "${UPDATES} New packages"
         elif (( UPDATES > 2 )); then
             notify-send -u low -i $NOTIFY_ICON \
-                "$UPDATES New packages"
+                "${UPDATES} New packages"
         fi
     fi
 
@@ -26,11 +26,9 @@ while true; do
     # every 10 seconds another check for updates is done
     while (( UPDATES > 0 )); do
         if (( UPDATES == 1 )); then
-            echo " $UPDATES Update"
+            echo "${BAR_ICON} ${UPDATES} Update"
         elif (( UPDATES > 1 )); then
-            echo " $UPDATES Updates"
-        else
-            echo $BAR_ICON
+            echo "${BAR_ICON} ${UPDATES} Updates"
         fi
         sleep 10
         get_total_updates
