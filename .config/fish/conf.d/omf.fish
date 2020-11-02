641c10206c41fa5deb06951efffc9753239c4fbb
Couapy <mael@marchand.cloud>
2020-11-02 14:43:47 +0100

    Migration to Arch Linux Update kitty and fish configurations

--- .config/fish/conf.d/omf.fish
+++ .config/fish/conf.d/omf.fish
@@ -7,4 +7,6 @@ set -q XDG_DATA_HOME
 source $OMF_PATH/init.fish
 
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias ll='ls -Alhv --group-directories-first'
 set VIRTUAL_ENV_DISABLE_PROMPT 1
+set theme_color_scheme nord
--- .config/fish/functions/n.fish
+++ .config/fish/functions/n.fish
@@ -1,6 +1,6 @@
 # Defined in /etc/fish/config.fish @ line 28
 function n
-	nnn -ae $argv
+	nnn -aeH $argv
     if test -e $NNN_TMPFILE
 		source $NNN_TMPFILE
     end
