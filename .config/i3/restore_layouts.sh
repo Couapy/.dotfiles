9be916e16d28a843e8b0facc607799faba3f9ab0
Couapy <mael@marchand.cloud>
2020-11-16 16:45:55 +0100

    Add Dunst configuration

--- .config/i3/restore_layouts.sh
+++ .config/i3/restore_layouts.sh
@@ -4,7 +4,4 @@ for layout in ~/.config/i3/layouts/*; do
     i3-msg "workspace $(basename "$layout" .json); append_layout $layout"
 done
 
-discord &
-/usr/bin/flatpak run --branch=stable --arch=x86_64 --command=spotify --file-forwarding com.spotify.Client &
-
 i3-msg "workspace 1"
