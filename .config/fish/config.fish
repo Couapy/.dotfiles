28995828b523033443f494a942262b43508ce3ab
Couapy <mael@marchand.cloud>
2021-06-01 10:39:30 +0200

    Update sudo prompt and aliases

---
+++ .config/fish/config.fish
@@ -0,0 +1,13 @@
+#!/usr/bin/fish
+
+# Configure fish
+set fish_greeting
+
+# Environnement configuration
+set -x SUDO_PROMPT "["(set_color blue)"sudo"(set_color normal)"]["(set_color green)"%p"(set_color normal)"->"(set_color yellow)"%U"(set_color normal)"] Password : "(set_color -r)"  secret "(set_color normal)
+
+# Aliases
+alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias ll='ls -Alhv --group-directories-first'
+alias gs='git status'
+alias gl='git lg'
